ae7b01251ef39ecd357f375409bf918272ccf97f
redha <mechekakredha@gmail.com>
2022-05-19 18:59:51 +0100

    orderitF5

--- config/settings.py
+++ config/settings.py
@@ -104,7 +104,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 DATABASES['default'] = dj_database_url.config()
