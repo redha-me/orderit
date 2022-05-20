d17de57047488ddfa5cfd8d9b5f68d3aee3fb244
redha <mechekakredha@gmail.com>
2022-05-20 10:09:44 +0100

    orderitF6

--- core/models.py
+++ core/models.py
@@ -2,12 +2,6 @@ import geocoder
 from django.db import models
 
 
-
-class Day(models.Model):
-	day=models.CharField(max_length=20,blank=False)
-
-	def __str__(self):
-		return self.day
 		
 class Hour(models.Model):
 	hour=models.CharField(max_length=6,blank=False)
